bc0f16e8028663aeaab710b889bcbb8a5143831f
tetrashine <tetra_shine@hotmail.com>
2025-05-29 14:33:02 +0800

    First working copy

--- client/app/src/components/App.vue
+++ client/app/src/components/App.vue
@@ -5,4 +5,5 @@ import NavBar from '@/components/NavBar.vue'
 <!-- App.vue -->
 <template>
     <NavBar />
+    <router-view />
 </template>
--- client/app/src/router/index.ts
+++ client/app/src/router/index.ts
@@ -1,19 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
 import HomeView from '@/features/home/views/Home.vue'
 import AboutView from '@/features/about/views/About.vue'
 import NotFoundView from '@/features/errors/views/NotFoundView.vue'
 
-const routes = [
+const routes: RouteRecordRaw[] = [
     {
         path: '/',
+        name: 'Root',
         redirect: 'home',
     },
     {
         path: '/home',
+        name: 'Home',
         component: HomeView,
     },
     {
         path: '/about',
+        name: 'About',
         component: AboutView,
     },
     {
